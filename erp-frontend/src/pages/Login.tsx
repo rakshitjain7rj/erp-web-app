4066bb770234762058d04889bca62faa5533553c
Manan <chawlamanan5@gmail.com>
2025-06-18 19:02:59 +0530

    messgae

--- erp-frontend/src/pages/Login.tsx
+++ erp-frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-
+// New Change
 const Login = () => {
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
