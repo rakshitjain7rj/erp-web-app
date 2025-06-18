709397f7cf5631119dc9c69722c7630a7a17d279
rakshitjain7rj <rakshitjain7rj@gmail.com>
2025-06-18 22:43:47 +0530

    is it uptodate check

--- server/models/Inventory.js
+++ server/models/Inventory.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// This model represents the inventory of products in the system.
 const inventorySchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   quantity: { type: Number, required: true },
