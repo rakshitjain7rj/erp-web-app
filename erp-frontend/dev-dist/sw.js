ded4e6136c19a79f228bd28b5c82cbec7677de70
Rakshit Jain <rakshitjain7rj@gmail.com>
2025-11-12 19:47:54 +0530

    fix(env): Update JWT expiration time to 30 days in production environment

--- erp-frontend/dev-dist/sw.js
+++ erp-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.6fh96g1h93g"
+    "revision": "0.ad32n8hhe9k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
