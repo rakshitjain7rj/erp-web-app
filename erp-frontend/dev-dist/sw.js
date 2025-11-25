679cea6d948b2d0d44088a0c8cc67e76b4ba5d2f
Rakshit Jain <rakshitjain7rj@gmail.com>
2025-11-25 11:02:24 +0530

    chore: Update service worker precache revision for index.html.

--- erp-frontend/dev-dist/sw.js
+++ erp-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.mksgnkavf4c"
+    "revision": "0.ldbasnhlc9k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
